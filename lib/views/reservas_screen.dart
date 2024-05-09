3f93d702b11dee53dcc9197deed49e15b4c85d30
KevinMnkey <kg739690@gmail.com>
2024-05-08 21:31:21 -0500

    pantalla ticket y ticket final

--- lib/views/reservas_screen.dart
+++ lib/views/reservas_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:proyecto_cinema/views/ticket_screen.dart';
 
 class ReservasPage extends StatefulWidget {
   const ReservasPage({super.key});
@@ -8,15 +9,138 @@ class ReservasPage extends StatefulWidget {
 }
 
 class _ReservasPageState extends State<ReservasPage> {
+  _ReservasPageState() {
+    _selectedVal = _productSizeList[0];
+  }
+
+  final _productSizeList = ["Small", "Medium", "Large", "Xlarge"];
+  String _selectedVal = "Small";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: BackButton(
-          color: Colors.orangeAccent,
+        backgroundColor: Colors.orangeAccent,
+        centerTitle: true,
+        title: const Text('CinemaPark'),
+        actions: <Widget>[],
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(25),
+                bottomRight: Radius.circular(25))),
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(top: 100.0, right: 20, left: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Nombres"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Apellidos"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Documento"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Telefono"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DropdownButtonFormField(
+                  value: _selectedVal,
+                  items: _productSizeList.map((e) {
+                    return DropdownMenuItem(
+                      child: Text(e),
+                      value: e,
+                    );
+                  }).toList(),
+                  onChanged: (val) {
+                    setState(() {
+                      _selectedVal = val as String;
+                    });
+                  },
+                  dropdownColor: Colors.deepPurple.shade50,
+                  decoration: InputDecoration(
+                      labelText: "Selecciona tu combo",
+                      border: OutlineInputBorder()),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DropdownButtonFormField(
+                  value: _selectedVal,
+                  items: _productSizeList.map((e) {
+                    return DropdownMenuItem(
+                      child: Text(e),
+                      value: e,
+                    );
+                  }).toList(),
+                  onChanged: (val) {
+                    setState(() {
+                      _selectedVal = val as String;
+                    });
+                  },
+                  dropdownColor: Colors.deepPurple.shade50,
+                  decoration: InputDecoration(
+                      labelText: "Selecciona el teatro",
+                      border: OutlineInputBorder()),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DropdownButtonFormField(
+                  value: _selectedVal,
+                  items: _productSizeList.map((e) {
+                    return DropdownMenuItem(
+                      child: Text(e),
+                      value: e,
+                    );
+                  }).toList(),
+                  onChanged: (val) {
+                    setState(() {
+                      _selectedVal = val as String;
+                    });
+                  },
+                  dropdownColor: Colors.deepPurple.shade50,
+                  decoration: InputDecoration(
+                      labelText: "Selecciona la función",
+                      border: OutlineInputBorder()),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                TicketFinalView()));
+                  },
+                  child: Text("Generar ticket"))
+            ],
+          ),
         ),
       ),
-      backgroundColor: Colors.blue[300],
     );
   }
 }
---
+++ lib/views/ticket_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TicketFinalView extends StatefulWidget {
+  const TicketFinalView({super.key});
+
+  @override
+  State<TicketFinalView> createState() => _TicketFinalViewState();
+}
+
+class _TicketFinalViewState extends State<TicketFinalView> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Ticket USUARIO #***");
+  }
+}
